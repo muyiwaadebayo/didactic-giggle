0184adad75bd6d72cff13019959ea02c8e782a63
muyiwaadebayo <61024607+muyiwaadebayo@users.noreply.github.com>
2021-09-17 18:27:16 +0100

    Update classify_images.py

--- classify_images.py
+++ classify_images.py
@@ -80,7 +80,7 @@ def classify_images(images_dir, results_dic, model):
 
        if truth in model_label:
            results_dic[[key][0]].extend([model_label, 1])
-           results_dic[[key][0]].extend([model_label, 1])
+       
 
        else:
            results_dic[[key][0]].extend([model_label, 0])
